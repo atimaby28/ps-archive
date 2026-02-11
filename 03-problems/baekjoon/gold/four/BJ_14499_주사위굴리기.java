1e5b5f001ebcee475938122df2f5ca892d29ab71
atimaby28 <atimaby28@gmail.com>
2026-02-11 23:29:14 +0900

    remove tracked iml and idea files

--- 03-problems/baekjoon/gold/four/BJ_14499_주사위굴리기.java
+++ 03-problems/baekjoon/gold/four/BJ_14499_주사위굴리기.java
@@ -1,4 +1,54 @@
 package baekjoon.gold.four;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class BJ_14499_주사위굴리기 {
+
+    static int N, M, K;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] commands = new int[K];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            commands[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = solution(x, y, commands);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int solution(int x, int y, int[] commands) {
+        
+    }
 }
