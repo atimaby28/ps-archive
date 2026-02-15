a1c632558bea9b8168c086b37d8dbb7e7cdb1e7b
atimaby28 <atimaby28@gmail.com>
2026-02-15 23:55:45 +0900

    update: simulation

--- 03-problems/baekjoon/gold/four/BJ_14499_주사위굴리기.java
+++ 03-problems/baekjoon/gold/four/BJ_14499_주사위굴리기.java
@@ -6,20 +6,25 @@ import java.util.StringTokenizer;
 public class BJ_14499_주사위굴리기 {
 
     static int N, M, K;
+    static int[] dice;
     static int[][] map;
+
+    static final int TOP = 0, EAST = 1, WEST = 2, NORTH = 3, SOUTH = 4, BOTTOM = 5;
+
+    static int[] dx = {0, 1, -1, 0, 0};
+    static int[] dy = {0, 0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
 
         K = Integer.parseInt(st.nextToken());
 
@@ -39,7 +44,7 @@ public class BJ_14499_주사위굴리기 {
             commands[i] = Integer.parseInt(st.nextToken());
         }
 
-        int result = solution(x, y, commands);
+        String result = solution(x, y, commands);
 
         bw.write(result + "\n");
 
@@ -48,7 +53,61 @@ public class BJ_14499_주사위굴리기 {
         br.close();
     }
 
-    private static int solution(int x, int y, int[] commands) {
-        
+    private static String solution(int x, int y, int[] commands) {
+        StringBuilder sb = new StringBuilder();
+
+        dice = new int[7];
+
+        for (int command : commands) {
+            int nx = x + dx[command];
+            int ny = y + dy[command];
+
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
+
+            rolling(command);
+
+            x = nx;
+            y = ny;
+
+            if (map[y][x] == 0) {
+                map[y][x] = dice[BOTTOM];
+            } else {
+                dice[BOTTOM] = map[y][x];
+                map[y][x] = 0;
+            }
+
+            sb.append(dice[TOP]).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private static void rolling(int direction) {
+        int top = dice[TOP], bottom = dice[BOTTOM], north = dice[NORTH],
+        east = dice[EAST], west = dice[WEST], south = dice[SOUTH];
+
+        switch (direction) {
+            case EAST -> {
+                dice[TOP] = west;
+                dice[EAST] = top;
+                dice[BOTTOM] = east;
+                dice[WEST] = bottom;
+            } case WEST -> {
+                dice[TOP] = east;
+                dice[EAST] = bottom;
+                dice[BOTTOM] = west;
+                dice[WEST] = top;
+            } case NORTH -> {
+                dice[TOP] = south;
+                dice[NORTH] = top;
+                dice[SOUTH] = bottom;
+                dice[BOTTOM] = north;
+            } case SOUTH -> {
+                dice[TOP] = north;
+                dice[NORTH] = bottom;
+                dice[SOUTH] = top;
+                dice[BOTTOM] = south;
+            }
+        }
     }
 }
---
+++ 03-problems/baekjoon/gold/four/BJ_3190_뱀.java
@@ -0,0 +1,111 @@
+package baekjoon.gold.four;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_3190_뱀 {
+
+    static int N, K, L;
+    static int[][] map;
+    static boolean[][] isSnake;
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    static Queue<TurnQueue> turnQueue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        map = new int[N + 1][N + 1];
+        isSnake = new boolean[N + 1][N + 1];
+        turnQueue = new ArrayDeque<>();
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            map[y][x] = 2;
+        }
+
+        L = Integer.parseInt(br.readLine());
+        for (int i = 0; i < L; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int time = Integer.parseInt(st.nextToken());
+            char direction = st.nextToken().charAt(0);
+
+            turnQueue.add(new TurnQueue(time, direction));
+        }
+
+        int result = solution();
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int solution() {
+        int time = 0;
+
+        Deque<int[]> snake = new ArrayDeque<>();
+
+        snake.add(new int[]{1, 1});
+        isSnake[1][1] = true;
+
+        int direction = 0;
+
+        while (true) {
+            int[] cur = snake.peekFirst();
+            int ny = cur[0] + dy[direction];
+            int nx = cur[1] + dx[direction];
+
+            if (nx < 1 || nx > N || ny < 1 || ny > N || map[ny][nx] == 1) break;
+
+            snake.addFirst(new int[]{ny, nx});
+
+            if (map[ny][nx] != 2) {
+                snake.pollLast();
+                isSnake[cur[0]][cur[1]] = false;
+            }
+
+            time++;
+
+            if (ny == snake.peekLast()[0] && nx == snake.peekLast()[1]) break;
+
+            isSnake[ny][nx] = true;
+
+            if (!turnQueue.isEmpty() && turnQueue.peek().time == time) {
+                if (turnQueue.peek().direction == 'D') direction = (direction + 1) % 4;
+                else direction = (direction + 3) % 4;
+
+            }
+        }
+
+        return time;
+    }
+
+    static class TurnQueue {
+        int time;
+        char direction;
+
+        public TurnQueue(int time, char direction) {
+            this.time = time;
+            this.direction = direction;
+        }
+    }
+}
