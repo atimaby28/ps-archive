d9578bc09b2752785de00adb0bc4de2bb40d1e94
atimaby28 <atimaby28@gmail.com>
2026-03-10 01:21:17 +0900

    update: Algorithm

--- 03-problems/baekjoon/gold/five/BJ_6593_상범빌딩.java
+++ 03-problems/baekjoon/gold/five/BJ_6593_상범빌딩.java
@@ -4,11 +4,19 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_6593_상범빌딩 {
     static int L, R, C;
     static char[][][] building;
+    static boolean[][][] visited;
+
+    // 상하(Level), 앞뒤(Row), 좌우(Col)
+    static int[] dl = {-1, 1,  0, 0,  0, 0};  // 아래층, 위층
+    static int[] dr = { 0, 0, -1, 1,  0, 0};  // 앞, 뒤
+    static int[] dc = { 0, 0,  0, 0, -1, 1};  // 왼, 오
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,6 +33,7 @@ public class BJ_6593_상범빌딩 {
             if (L == 0 && R == 0 && C == 0) break;
 
             building = new char[L][R][C];
+            visited = new boolean[L][R][C];
 
             int startL = 0, startR = 0, startC = 0;
             int endL = 0, endR = 0, endC = 0;
@@ -49,7 +58,7 @@ public class BJ_6593_상범빌딩 {
                 br.readLine(); // 각 층 사이의 빈 줄 처리
             }
 
-            sb.append(solution(startL, endL, startR, endR, startC, endC)).append("\n");
+            sb.append(solution(startL, startR, startC, endL, endR, endC)).append("\n");
         }
 
         bw.write(sb.toString());
@@ -59,7 +68,42 @@ public class BJ_6593_상범빌딩 {
         br.close();
     }
 
-    private static String solution(int startL, int endL, int startR, int endR, int startC, int endC) {
-        return "";
+    private static String solution(int startL, int startR, int startC, int endL, int endR, int endC) {
+        // #: 지나갈 수 없는 칸, '.': 비어 있는 칸
+        // 도착 지점이 있기 때문에 큐 배엘어 넣어도 레벨 단위 가능
+        return bfs(startL, startR, startC, endL, endR, endC);
+    }
+
+    private static String bfs(int startL, int startR, int startC, int endL, int endR, int endC) {
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        queue.offer(new int[]{startL, startR, startC, 0});
+        visited[startL][startR][startC] = true;
+
+        int minutes = 0;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            int curL = cur[0];
+            int curR = cur[1];
+            int curC = cur[2];
+            int curMinutes = cur[3];
+
+            if (curL == endL && curR == endR && curC == endC) return "Escaped in " + curMinutes + " minute(s).";
+
+            for (int d = 0; d < 6; d++) {
+                int newL = curL + dl[d];
+                int newR = curR + dr[d];
+                int newC = curC + dc[d];
+
+                if (newL < 0 || newL >= L || newR < 0 || newR >= R || newC < 0 || newC >= C) continue;
+                if (visited[newL][newR][newC] || building[newL][newR][newC] == '#') continue;
+
+                visited[newL][newR][newC] = true;
+                queue.offer(new int[]{newL, newR, newC, curMinutes + 1});
+            }
+        }
+
+        return "Trapped!";
     }
 }
--- 03-problems/baekjoon/gold/three/BJ_1600_말이되고픈원숭이.java
+++ 03-problems/baekjoon/gold/three/BJ_1600_말이되고픈원숭이.java
@@ -4,11 +4,17 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_1600_말이되고픈원숭이 {
     static int K, W, H;
     static int[][] map;
+    static boolean[][][] visited;
+
+    static int[] dr = {-1, 1, 0, 0, -1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] dc = {0, 0, -1, 1, -2, -1, 1, 2, 2, 1, -1, -2};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +28,7 @@ public class BJ_1600_말이되고픈원숭이 {
         H = Integer.parseInt(st.nextToken());
 
         map = new int[H][W];
+        visited = new boolean[H][W][K + 1];
 
         for (int r = 0; r < H; r++) {
             st = new StringTokenizer(br.readLine());
@@ -41,6 +48,45 @@ public class BJ_1600_말이되고픈원숭이 {
     }
 
     private static int solution() {
+        // 0: 평지, 1: 장애물
+        return bfs(0, 0);
+    }
+
+    private static int bfs(int r, int c) {
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        queue.offer(new int[]{r, c, 0, 0});
+        visited[r][c][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            int curR = cur[0];
+            int curC = cur[1];
+            int curK = cur[2];
+            int curMove = cur[3];
+
+            if (curR == H - 1 && curC == W - 1) return curMove;
+
+            for (int d = 0; d < 12; d++) {
+                int newR = curR + dr[d];
+                int newC = curC + dc[d];
+
+                if (newR < 0 || newR >= H || newC < 0 || newC >= W) continue;
+                if (curK >= K && d >= 4) break;
+
+                if (curK < K && d >= 4) {
+                    if (map[newR][newC] == 1 || visited[newR][newC][curK + 1]) continue;
+                    visited[newR][newC][curK + 1] = true;
+                    queue.offer(new int[]{newR, newC, curK + 1, curMove + 1});
+                } else {
+                    if (map[newR][newC] == 1 || visited[newR][newC][curK]) continue;
+                    visited[newR][newC][curK] = true;
+                    queue.offer(new int[]{newR, newC, curK, curMove + 1});
+                }
+            }
+        }
+
         return -1;
     }
 }
