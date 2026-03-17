d1b50e779e2ac1ba531cd07d8fe82be3319069b3
atimaby28 <atimaby28@gmail.com>
2026-03-18 01:58:43 +0900

    update: Algorithm

--- 03-problems/baekjoon/silver/one/BJ_11660_구간합구하기5.java
+++ 03-problems/baekjoon/silver/one/BJ_11660_구간합구하기5.java
@@ -7,28 +7,42 @@ public class BJ_11660_구간합구하기5 {
 
     static int N, M;
     static int[][] array;
+    static int[][] prefixSum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        array = new int[N][M];
+        array = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 0; j < N; j++) {
                 array[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        String result = solution();
+        solution();
 
-        bw.write(result + "\n");
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int sRow = Integer.parseInt(st.nextToken());
+            int sCol = Integer.parseInt(st.nextToken());
+            int eRow = Integer.parseInt(st.nextToken());
+            int eCol = Integer.parseInt(st.nextToken());
+
+            sb.append(prefixSum[eRow][eCol] - prefixSum[sRow - 1][eCol] - prefixSum[eRow][sCol - 1] + prefixSum[sRow - 1][sCol - 1]).append("\n");
+
+        }
+
+        bw.write(sb.toString());
 
         bw.flush();
 
@@ -36,10 +50,13 @@ public class BJ_11660_구간합구하기5 {
         br.close();
     }
 
-    private static String solution() {
-        StringBuilder sb = new StringBuilder();
+    private static void solution() {
+        prefixSum = new int[N + 1][N + 1];
 
-
-        return null;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + array[i - 1][j - 1];
+            }
+        }
     }
 }
--- 03-problems/baekjoon/silver/one/BJ_9465_스티커.java
+++ 03-problems/baekjoon/silver/one/BJ_9465_스티커.java
@@ -21,14 +21,14 @@ public class BJ_9465_스티커 {
 
             int[][] array = new int[2][n];
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < 2; i++) {
                 st = new StringTokenizer(br.readLine());
-
-                array[0][i] = Integer.parseInt(st.nextToken());
-                array[1][i] = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < n; j++) {
+                    array[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
 
-            sb.append(solution(n, array));
+            sb.append(solution(n, array)).append("\n");
 
         }
 
@@ -41,6 +41,20 @@ public class BJ_9465_스티커 {
     }
 
     private static int solution(int n, int[][] array) {
-        return 0;
+        int[][] dp = new int[2][n];
+
+        if (n == 1) return Math.max(array[0][0], array[1][0]);
+
+        dp[0][0] = array[0][0];
+        dp[1][0] = array[1][0];
+        dp[0][1] = array[1][0] + array[0][1];
+        dp[1][1] = array[0][0] + array[1][1];
+
+        for (int i = 2; i < n; i++) {
+            dp[0][i] = array[0][i] + Math.max(dp[1][i - 1], dp[1][i - 2]);
+            dp[1][i] = array[1][i] + Math.max(dp[0][i - 1], dp[0][i - 2]);
+        }
+
+        return Math.max(dp[0][n - 1], dp[1][n - 1]);
     }
 }
