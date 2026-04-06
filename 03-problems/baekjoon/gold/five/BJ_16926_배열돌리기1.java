70f4d1acfc64ea7ec0a9cee25cabb7c52881824e
atimaby28 <atimaby28@gmail.com>
2026-04-06 23:32:51 +0900

    update: Algorithm

--- 03-problems/baekjoon/gold/five/BJ_16926_배열돌리기1.java
+++ 03-problems/baekjoon/gold/five/BJ_16926_배열돌리기1.java
@@ -4,9 +4,8 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class BJ_16926_배열돌리기1 {
-
     static int N, M, R;
-    static int[][] array;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,11 +20,11 @@ public class BJ_16926_배열돌리기1 {
         for (int i = 0; i < N; i++) {
             st =  new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                array[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int result = solution();
+        String result = solution();
 
         bw.write(result + "\n");
 
@@ -35,10 +34,37 @@ public class BJ_16926_배열돌리기1 {
         br.close();
     }
 
-    private static int solution() {
-        while (R-- > 0) {
+    private static String solution() {
+        StringBuilder sb = new StringBuilder();
 
-            
+        int layers = Math.min(N, M) / 2;
+
+        for (int r = 0; r < R; r++) {
+            for (int l = 0; l < layers; l++) {
+                int top = l, left = l;
+                int bottom = N - 1 - l, right = M - 1 - l;
+                int temp = map[top][left];
+
+                // 왼쪽 열 위로
+                for (int i = top; i < bottom; i++) map[i][left] = map[i + 1][left];
+                // 아래쪽 행 왼쪽으로
+                for (int j = left; j < right; j++) map[bottom][j] = map[bottom][j + 1];
+                // 오른쪽 열 아래로
+                for (int i = bottom; i > top; i--) map[i][right] = map[i - 1][right];
+                // 위쪽 행 오른쪽으로
+                for (int j = right; j > left + 1; j--) map[top][j] = map[top][j - 1];
+                map[top][left + 1] = temp;
+            }
         }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (j > 0) sb.append(' ');
+                sb.append(map[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        return sb.toString();
     }
 }
--- 03-problems/baekjoon/gold/five/BJ_16927_배열돌리기2.java
+++ 03-problems/baekjoon/gold/five/BJ_16927_배열돌리기2.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class BJ_16927_배열돌리기2 {
     static int N, M, R;
-    static int[][] array;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,11 +20,11 @@ public class BJ_16927_배열돌리기2 {
         for (int i = 0; i < N; i++) {
             st =  new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                array[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int result = solution();
+        String result = solution();
 
         bw.write(result + "\n");
 
@@ -34,10 +34,35 @@ public class BJ_16927_배열돌리기2 {
         br.close();
     }
 
-    private static int solution() {
-        while (R-- > 0) {
+    private static String solution() {
+        StringBuilder sb = new StringBuilder();
 
+        int layers = Math.min(N, M) / 2;
 
+        for (int l = 0; l < layers; l++) {
+            int top = l, left = l;
+            int bottom = N - 1 - l, right = M - 1 - l;
+            int perimeter = 2 * (bottom - top + right - left);
+            int rot = R % perimeter;
+
+            for (int r = 0; r < rot; r++) {
+                int temp = map[top][left];
+                for (int i = top; i < bottom; i++) map[i][left] = map[i + 1][left];
+                for (int j = left; j < right; j++) map[bottom][j] = map[bottom][j + 1];
+                for (int i = bottom; i > top; i--) map[i][right] = map[i - 1][right];
+                for (int j = right; j > left + 1; j--) map[top][j] = map[top][j - 1];
+                map[top][left + 1] = temp;
+            }
         }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (j > 0) sb.append(' ');
+                sb.append(map[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        return sb.toString();
     }
 }
