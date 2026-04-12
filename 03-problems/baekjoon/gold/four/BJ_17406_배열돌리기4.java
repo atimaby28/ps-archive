f626fa9d43719e6a269b8a4f6ab51373be29708e
atimaby28 <atimaby28@gmail.com>
2026-04-13 01:27:32 +0900

    update: Algorithm

--- 03-problems/baekjoon/gold/four/BJ_17406_배열돌리기4.java
+++ 03-problems/baekjoon/gold/four/BJ_17406_배열돌리기4.java
@@ -1,12 +1,16 @@
 package baekjoon.gold.four;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BJ_17406_배열돌리기4 {
 
-    static int N, M, K, A;
-    static int[][] array, command;
+    static int N, M, K, A = Integer.MAX_VALUE;
+    static int[][] array;
+    static List<int[]> commands;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +23,7 @@ public class BJ_17406_배열돌리기4 {
         K = Integer.parseInt(st.nextToken());
 
         array = new int[N][M];
-        command = new int[K][3];
+        commands = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -30,9 +34,7 @@ public class BJ_17406_배열돌리기4 {
 
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            command[i][0] = Integer.parseInt(st.nextToken());
-            command[i][1] = Integer.parseInt(st.nextToken());
-            command[i][2] = Integer.parseInt(st.nextToken());
+            commands.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
 
         int result = solution();
@@ -46,6 +48,74 @@ public class BJ_17406_배열돌리기4 {
     }
 
     private static int solution() {
-        
+        permutation(0, new ArrayList<>(), new boolean[K]);
+
+        return A;
+    }
+
+    private static void permutation(int depth, List<int[]> result, boolean[] visited) {
+        if (depth == K) {
+            int[][] copiedArray = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                copiedArray[i] = array[i].clone();
+            }
+
+            A = Math.min(A, rotating(copiedArray, result));
+
+            return;
+        }
+
+        for (int i = 0; i < K; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            result.add(commands.get(i));
+            permutation(depth + 1, result, visited);
+            result.remove(result.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+    private static int rotating(int[][] copiedArray, List<int[]> result) {
+        // (r - s, c - s), (r + s, c + s);
+
+        int[] minRow = new int[N];
+
+        for (int[] command : result) {
+            int startRow = command[0] - command[2] - 1;
+            int startCol = command[1] - command[2] - 1;
+            int endRow = command[0] + command[2] - 1;
+            int endCol = command[1] + command[2] - 1;
+
+            int layers =  Math.min(endRow - startRow, endCol - startCol) / 2;
+
+            for (int l = 0; l < layers; l++) {
+                int top = startRow + l, left = startCol + l;
+                int bottom = endRow - l, right = endCol - l;
+
+                int temp = copiedArray[top][right];
+
+                // →
+                for (int j = right; j > left; j--) copiedArray[top][j] = copiedArray[top][j - 1];
+                // ↑
+                for (int j = top; j < bottom; j++) copiedArray[j][left] = copiedArray[j + 1][left];
+                // ←
+                for (int j = left; j < right; j++) copiedArray[bottom][j] = copiedArray[bottom][j + 1];
+                // ↓
+                for (int j = bottom; j > top; j--) copiedArray[j][right] = copiedArray[j - 1][right];
+
+                copiedArray[top + 1][right] = temp;
+            }
+
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                minRow[i] += copiedArray[i][j];
+            }
+        }
+
+        Arrays.sort(minRow);
+
+        return minRow[0];
     }
 }
